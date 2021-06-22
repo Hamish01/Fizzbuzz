4e851d815c1efab712b4d37029d9ff2394b3061c
Hamish Sansom <79813180+Hamish01@users.noreply.github.com>
2021-06-22 13:04:58 +1000

    comment

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fizzbop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 0;
+            while (Number != 100)
+            {
+                Number++;
+                if (Number % 15 == 0){
+                    Console.WriteLine("fizzbuzz");
+                }
+                else{
+                    if(Number % 5 == 0){
+                        Console.WriteLine("Buzz");
+                    }
+                    else{
+                        if (Number % 3 == 0){
+                            Console.WriteLine("Fizz");
+                        }
+                        else{
+                            Console.WriteLine(Number);
+                        }
+                    }
+        
+                }
+            }
+        }
+    }
+}
